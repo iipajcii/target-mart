a49a40bc5a6850a485f258a854ed0789f58099f7
Patrick <iipajcii@gmail.com>
2020-12-04 13:35:23 -0500

    Added Carousel and Vue-Router to project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,32 @@
     <div id="app">
         <carousel/>
     </div>
+{{--     <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
+ --}}
+    <div id="vue-router">
+      <h1>Hello App!</h1>
+      <p>
+        <!-- use router-link component for navigation. -->
+        <!-- specify the link by passing the `to` prop. -->
+        <!-- <router-link> will be rendered as an `<a>` tag by default -->
+        <router-link to="/foo">Go to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+      </p>
+      <!-- route outlet -->
+      <!-- component matched by the route will render here -->
+      <router-view></router-view>
+    </div>
+    <style type="text/css">
+        .router-link-active {
+          color: red;
+        }
+    </style>
+    Jamart, Jamaica's First Social Online Marketplace
 @endsection
 @section('scripts')
     <script src="{{asset('/js/index.js')}}"></script>
+    <script type="text/javascript">
+
+    </script>
 @endsection
