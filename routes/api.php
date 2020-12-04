a2ecdd73ddb6bd7a2cbd3194ed38786035618336
Patrick <iipajcii@gmail.com>
2020-12-04 18:03:43 -0500

    Including Products routes in routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('create-product', function (Request $request){
+    if($request->image){$path = $request->file('image')->store('public/showcase-images');$request->image = $path;}
+    Product::add_product($request);
+    return $request;
+})->name('api-create-product');
+Route::post('edit-product', function (Request $request){
+    if($request->image){$path = $request->file('image')->store('public/showcase-images');$request->image = $path;}
+    Product::edit_product($request);
+    return $request;
+})->name('api-edit-product');
+Route::post('toggle-product', function (Request $request){
+    return Product::toggle_product($request);
+})->name('api-toggle-product');
+
+Route::get('products',function(){return Product::all();})->name('api-products');
