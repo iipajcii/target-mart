3b8a02b0c8b7dac79de297c4f5c7be778e05f2c2
Patrick <iipajcii@gmail.com>
2020-12-05 10:04:38 -0500

    Made the images loaded by this component random from the 'picsum' website

--- resources/js/vue/buefy/carousel.vue
+++ resources/js/vue/buefy/carousel.vue
@@ -1,26 +1,95 @@
 <template>
-    <b-carousel>
-        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
-            <section :class="`hero is-medium is-${carousel.color}`">
-                <div class="hero-body has-text-centered">
-                    <h1 class="title">{{carousel.text}}</h1>
-                </div>
-            </section>
+    <b-carousel
+        :autoplay="false"
+        with-carousel-list
+        :indicator="false"
+        :overlay="gallery"
+        @click="switchGallery(true)">
+        <b-carousel-item v-for="(item, i) in items" :key="i">
+            <figure class="image">
+                <img :src="item.image">
+            </figure>
         </b-carousel-item>
+        <span v-if="gallery" @click="switchGallery(false)" class="modal-close is-large"/>
+        <template slot="list" slot-scope="props">
+            <b-carousel-list
+                v-model="props.active"
+                :data="items"
+                v-bind="al"
+                @switch="props.switch($event, false)"
+                as-indicator />
+        </template>
+        <template slot="overlay">
+            <div class="has-text-centered has-text-white">
+                Hello i'am overlay!
+            </div>
+        </template>
     </b-carousel>
 </template>
 
 <script>
 export default {
-    data(){
+    data() {
         return {
-            carousels: [
-                { text: 'Slide 1', color: 'primary' },
-                { text: 'Slide 2', color: 'info' },
-                { text: 'Slide 3', color: 'success' },
-                { text: 'Slide 4', color: 'warning' },
-                { text: 'Slide 5', color: 'danger' }
+            gallery: false,
+            al: {
+                hasGrayscale: true,
+                itemsToShow: 2,
+                breakpoints: {
+                    768: {
+                        hasGrayscale: false,
+                        itemsToShow: 4
+                    },
+                    960: {
+                        hasGrayscale: true,
+                        itemsToShow: 6
+                    }
+                }
+            },
+            items: [
+                {
+                    title: 'Slide 1',
+                    image: 'https://picsum.photos/1230/500?random=1'
+                },
+                {
+                    title: 'Slide 2',
+                    image: 'https://picsum.photos/1230/500?random=2'
+                },
+                {
+                    title: 'Slide 3',
+                    image: 'https://picsum.photos/1230/500?random=3'
+                },
+                {
+                    title: 'Slide 4',
+                    image: 'https://picsum.photos/1230/500?random=4'
+                },
+                {
+                    title: 'Slide 5',
+                    image: 'https://picsum.photos/1230/500?random=5'
+                },
+                {
+                    title: 'Slide 6',
+                    image: 'https://picsum.photos/1230/500?random=6'
+                },
+                {
+                    title: 'Slide 7',
+                    image: 'https://picsum.photos/1230/500?random=7'
+                },
+                {
+                    title: 'Slide 8',
+                    image: 'https://picsum.photos/1230/500?random=8'
+                }
             ]
+        }
+    },
+    methods: {
+        switchGallery(value) {
+            this.gallery = value
+            if (value) {
+                document.documentElement.classList.add('is-clipped')
+            } else {
+                document.documentElement.classList.remove('is-clipped')
+            }
         }
     }
 }
