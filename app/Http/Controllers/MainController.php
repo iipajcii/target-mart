91f98b46aa7566fd697c39cd97b8b75ac30f8cd8
Patrick <iipajcii@gmail.com>
2020-12-04 21:28:05 -0500

    Updated Routes and MainController to map to the main parts of the application.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Action;
+use App\Models\Product;
 
 class MainController extends Controller
 {
     public function index(Request $request)
     {
-        return view('index');
+        return view('index')
+            ->with("recent",Product::recent())
+        ;
     }
 
     public function blog(Request $request)
@@ -21,13 +26,32 @@ class MainController extends Controller
         return view('search');
     }
 
-    public function product(Request $request)
+    public function products(Request $request)
     {
-        return view('product');
+        return view('products');
+    }
+
+    public function product(Request $request, $id)
+    {
+        return view('product')->with('product',Product::findOrFail($id));
     }
 
     public function purchase(Request $request)
     {
         return view('purchase');
+    }
+
+    public function login(Request $request)
+    {
+        return view('login');
+    }
+
+    public function dashboard(Request $request)
+    {
+        $user = User::where('name',$request->username)->where('password',$request->password)->first();
+        if(!$user){return redirect('login');}
+        else{
+            return view('dashboard')->with('recent',Action::recent())->with('user',$user)->with('products',Product::get());
+        }
     }
 }
