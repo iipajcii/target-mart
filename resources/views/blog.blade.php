9867fcefcad9f6398cf8dd5eceff9a856deab932
Patrick <iipajcii@gmail.com>
2020-12-05 02:29:23 -0500

    Created the Comming Soon for the Blog Page

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.default')
+@section('title',"Blog")
+@section('content')
+    <div class="columns">
+        <div class="column">
+            <div class="box has-text-centered m-4">
+                <h1 class="is-size-3">Blog</h1>
+                <hr/>
+                <h2 class="is-size-4">Coming Soon</h2>
+            </div>
+        </div>
+    </div>
+@endsection
