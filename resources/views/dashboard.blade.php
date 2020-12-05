d6511c00e5b32f00c38122370130e5a878e9cb63
Patrick <iipajcii@gmail.com>
2020-12-04 21:26:54 -0500

    This is the dashboard where tasks such as adding, editing, and hiding products will be done

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bulma.css')}}">
+    <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
+</head>
+<body>
+    <style type="text/css">
+        .is-hidden {display: none}
+        .max-95 {
+            max-width: 95%;
+            margin: auto;
+        }
+    </style>
+    <script type="text/javascript">
+        function toggleTab(element){
+            document.getElementsByName('edit-entry-main')[0].classList.remove('is-hidden');
+
+            let viewElements = document.getElementById('view').children;
+            let name = element.getAttribute("name");
+            let elements = document.getElementsByName(name);
+            let tabs = element.parentElement.children;
+
+
+
+            for(let counter = 0, count = viewElements.length; counter < count; counter++)
+            {
+                viewElements[counter].classList.add('is-hidden');
+                tabs[counter].classList.remove('is-active');
+            }
+            element.classList.toggle('is-active');
+            for(let counter = 0, count = elements.length; counter < count; counter++)
+            {
+                if(elements[counter].getAttribute('name') == name){
+                    elements[counter].classList.remove('is-hidden');
+                }
+            }
+            for(counter = 1; document.getElementsByName('edit-entry-'+counter)[1]; counter++)
+            {
+                if(document.getElementsByName('edit-entry-'+counter)[1].tagName.toLowerCase() == 'div'){
+                    console.log(counter);
+                    document.getElementsByName('edit-entry-'+counter)[1].classList.add('is-hidden');
+                }
+
+            }
+        }
+
+        function toggleEditor(element) {
+            let viewElements = document.getElementById('edit-entry-pages').children;
+            let name = element.getAttribute("name");
+            let elements = document.getElementsByName(name);
+            console.log(elements);
+
+            for(let counter = 0, count = viewElements.length; counter < count; counter++)
+            {
+                viewElements[counter].classList.add('is-hidden');
+            }
+            for(let counter = 0, count = elements.length; counter < count; counter++)
+            {
+                if(elements[counter].getAttribute('name') == name){
+                    elements[counter].classList.remove('is-hidden');
+                }
+            }
+            document.getElementsByName('edit-entry-main')[0].classList.add('is-hidden');
+
+        }
+
+        function displayImage(imgElement){
+            var reader = new FileReader();
+            reader.onload = function()
+            {
+                var output = document.getElementById(imgElement);
+                output.src = reader.result;
+            };
+            reader.readAsDataURL(event.target.files[0]);
+            document.getElementById('image-name').innerHTML = document.getElementsByName('image')[0].files[0].name;
+        };
+
+        function displayEditorImage(imgElement){
+            var reader = new FileReader();
+            reader.onload = function()
+            {
+                var output = document.getElementById(imgElement);
+                output.src = reader.result;
+            };
+            reader.readAsDataURL(event.target.files[0]);
+            // document.getElementById('image-name').innerHTML = document.getElementsByName('image')[0].files[0].name;
+        };
+
+        function createXMLObject()
+        {
+            var xmlHttp;
+            if(window.ActiveXObject)
+            {
+                try { xml = new ActiveXObject("Microsoft.XMLHTTP"); }
+                catch(e) { xmlHttp = false; }
+            }
+            else
+            {
+                try { xmlHttp = new XMLHttpRequest(); }
+                catch (e) { xmlHttp = false; }
+            }
+            if(!xmlHttp){ alert("Error creating XMLHttpRequest Object"); }
+            else { return xmlHttp; }
+        }
+
+        function toggleVisibility(element, link) {
+            var ajax = createXMLObject();
+            if(ajax.readyState == 4 || ajax.readyState == 0)
+            {
+                ajax.open("POST",link,true);
+                ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                ajax.setRequestHeader('X-CSRF-TOKEN', "{{ csrf_token() }}"); //Token is on index page
+
+                ajax.onreadystatechange = function() {
+                    if(ajax.readyState == 4) {
+                        if(ajax.status == 200)
+                        {
+                            console.log(ajax.responseText);
+                            return ajax.responseText;
+                            // setTimeout('ajaxProcess()',2000);
+                        }
+                    }
+                    else
+                    {
+                        console.log("Connecting...");
+                    }
+                }
+                var data = element.getAttribute('entryId');
+                if (data) {ajax.send("data=" + data);}
+                else {ajax.send();}
+            }
+        }
+    </script>
+    <section class="hero is-primary is-bold mb-4">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-size-2">
+            Dashboard
+          </h1>
+          <h2 class="subtitle">
+            The administrator dashboard for showcaser
+          </h2>
+        </div>
+      </div>
+    </section>
+    <div class="tabs is-centered">
+      <ul>
+        <li class="is-active" name="home" onclick="toggleTab(this)"><a>Home</a></li>
+        <li name="create-entry" onclick="toggleTab(this)"><a>Create Entry</a></li>
+        <li name="edit-entry" onclick="toggleTab(this)"><a>Edit Entry</a></li>
+        <li name="toggle-entry" onclick="toggleTab(this)"><a>Toggle Entry</a></li>
+      </ul>
+    </div>
+    <div class="columns" id="view">
+        {{-- Home View Start --}}
+        <div class="column is-full has-text-centered" name="home">
+            <h1 class="is-size-3  mb-4">Recent Activity</h1>
+            <hr class="mb-5" style="width: 70%; background-color: #DDD; height: 1px; margin: auto;">
+            <div class="columns is-multiline">
+                @foreach($recent as $activity)<div class="column is-full"><span>{{$activity}}</span></div>@endforeach
+            </div>
+        </div>
+        {{-- Home View Stop --}}
+
+        {{-- Create Entry Start --}}
+        <div class="column is-full has-text-centered is-hidden" name="create-entry">
+            <h1 class="is-size-3  mb-4">Create Entry</h1>
+            <hr class="mb-5" style="width: 70%; background-color: #DDD; height: 1px; margin: auto;">
+            <div class="columns is-centered">
+                <div class="column is-7">
+                    <form method="post" action="{{route('api-create-product')}}" enctype="multipart/form-data" >
+                        <div class="field">
+                            <label class="label">Name</label>
+                            <div class="control">
+                                <input class="input" type="text" placeholder="e.g The New Awesome Product" name="name"/>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">Product Display Image</label>
+                            <img id='product-showcase-image' src="https://bulma.io/images/placeholders/1280x960.png"/>
+                            <div class="file has-name is-fullwidth">
+                            <label class="file-label">
+                                <input class="file-input" type="file" name="image" onchange="displayImage('product-showcase-image')" required>
+                                <span class="file-cta">
+                                    <span class="file-icon">
+                                        <i class="fas fa-upload"></i>
+                                    </span>
+                                    <span class="file-label">Choose a file…</span>
+                                </span>
+                                <span class="file-name" id="image-name">Default Image is currently selected</span>
+                            </label>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">Product Description</label>
+                            <div class="control">
+                                <textarea class="textarea" placeholder="e.g. This is the awesome product &#10That is available for purchase!" rows="6" name="description"></textarea>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">Categories</label>
+                            <div class="control">
+                                <input class="input" type="text" placeholder="e.g. School+Business+Blog" name="categories" />
+                            </div>
+                            <p class="help">Add a '+' for each category there is</p>
+                        </div>
+                        <div class="control">
+                            <label>Is Hidden?</label><br/>
+                            <label class="radio">
+                                <input type="radio" name="hidden" value=1 checked> Yes
+                            </label>
+                            <label class="radio">
+                                <input type="radio" name="hidden" value=0> No
+                            </label>
+                        </div>
+                        <div class="field">
+                            <p class="control">
+                                <button class="button is-success">Submit Entry</button>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        {{-- Create Entry Stop  --}}
+
+        {{-- Edit Entry Start --}}
+        <div class="column is-full has-text-centered is-hidden"  style="z-index: 1" name="edit-entry">
+            <div name="edit-entry-main">
+                <h1 class="is-size-3  mb-4">Edit Entry</h1>
+                <hr class="mb-5" style="width: 70%; background-color: #DDD; height: 1px; margin: auto;">
+                <div class="columns is-multiline">
+                {{-- One Line Component--}}@foreach($products as $product)<div class="column is-4"><div class="card max-95"><div class="card-image"><picture class="image is-square"><source srcset="{{asset(str_replace('public','storage',$product->image).'.square.webp')}}" type="image/webp"><img src="{{asset(str_replace('public','storage',$product->image).'.square.jpeg')}}" type="image/jpeg"alt="Product Image"></picture></div><div class="card-content"><div class="media"><div class="media-left"></div><div class="media-content"><p class="title is-4">{{$product->name}}</p><p class="subtitle is-6">@foreach(explode("+",$product->categories) as $tag)<span class="tag is-info mr-1">{{$tag}}</span>@endforeach</p></div></div><div class="content">{{$product->description}}<br><div class="columns mt-3"><div class="column"><button class="button is-primary m-2" style="width: 100%;" onclick="toggleEditor(this)" name="edit-entry-{{$product->id}}">Edit Details</button></div></div></div></div></div></div>@endforeach
+                </div>
+            </div>
+            <div class="columns" id="edit-entry-pages">
+                {{-- Edit Entry Pages Start --}}
+
+            @foreach($products as $product)
+            <div class="column is-full has-text-centered is-hidden" style="z-index: 2" name="edit-entry-{{$product->id}}">
+                <h1 class="is-size-3  mb-4">Edit Entry</h1>
+                <hr class="mb-5" style="width: 70%; background-color: #DDD; height: 1px; margin: auto;">
+                <div class="columns is-centered">
+                    <div class="column is-7">
+                        <form method="post" action="{{route('api-edit-product')}}" enctype="multipart/form-data" >
+                            <input class="input" type="hidden" placeholder="e.g The New Awesome Product" name="id" value="{{$product->id}}" />
+                            <div class="field">
+                                <label class="label">Name</label>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="e.g The New Awesome Product" name="name" value="{{$product->name}}" />
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">product Display Image</label>
+                                <img id='product-showcase-image-{{$product->id}}' src="{{asset(str_replace('public','storage',$product->image))}}"/>
+                                <div class="file has-name is-fullwidth">
+                                <label class="file-label">
+                                    <input class="file-input" type="file" name="image" onchange="displayEditorImage('product-showcase-image-{{$product->id}}',{{$product->id}})">
+                                    <span class="file-cta">
+                                        <span class="file-icon">
+                                            <i class="fas fa-upload"></i>
+                                        </span>
+                                        <span class="file-label">Choose a file…</span>
+                                    </span>
+                                    <span class="file-name" name="image-name-{{$product->id}}">Default Image is currently selected</span>
+                                </label>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">product Description</label>
+                                <div class="control">
+                                    <textarea class="textarea" placeholder="e.g. This is the awesome product &#10That is available for purchase!" rows="6" name="description">{{$product->description}}</textarea>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="label">Categories</label>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="e.g. School+Business+Blog" name="categories" value="{{$product->categories}}" />
+                                </div>
+                                <p class="help">Add a '+' for each category there is</p>
+                            </div>
+                            <div class="control">
+                                <label>Is Hidden?</label><br/>
+                                <label class="radio">
+                                    <input type="radio" name="hidden" value=1 @if($product['is-hidden']) checked @endif> Yes
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="hidden" value=0 @if(!$product['is-hidden']) checked @endif> No
+                                </label>
+                            </div>
+                            <div class="field">
+                                <p class="control">
+                                    <button class="button is-success">Update Entry</button>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+
+        {{-- Edit Entry Pages Stop  --}}
+            </div>
+        </div>
+
+        {{-- Edit Entry Stop  --}}
+
+        {{-- Toggle Entry Start --}}
+        <div class="column is-full has-text-centered is-hidden" name="toggle-entry">
+            <h1 class="is-size-3 mb-4">Toggle Entry</h1>
+            <hr class="mb-5" style="width: 70%; background-color: #DDD; height: 1px; margin: auto;">
+            <div class="columns is-multiline">
+            {{-- One Line Component--}}@foreach($products as $product)<div class="column is-4"><div class="card max-95"><div class="card-image"><picture class="image is-square"><source srcset="{{asset(str_replace('public','storage',$product->image).'.square.webp')}}" type="image/webp"><img src="{{asset(str_replace('public','storage',$product->image).'.square.jpeg')}}" type="image/jpeg"alt="product Image"></picture></div><div class="card-content"><div class="media"><div class="media-left"></div><div class="media-content"><p class="title is-4">{{$product->name}}</p><p class="subtitle is-6">@foreach(explode("+",$product->categories) as $tag)<span class="tag is-info mr-1">{{$tag}}</span>@endforeach</p></div></div><div class="content">{{$product->description}}<br><div class="columns mt-3"><div class="column"><button class="button is-primary m-2" style="width: 100%;" name="toggle-entry-{{$product->id}}" entryId="{{$product->id}}" onclick="toggleVisibility(this,'{{route("api-toggle-product")}}')">Toggle Visibility</button></div></div></div></div></div></div>@endforeach
+            </div>
+        </div>
+        {{-- Toggle Entry Stop  --}}
+
+    </div>
+</body>
+</html>
